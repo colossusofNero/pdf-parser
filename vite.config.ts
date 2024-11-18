0483b9b15c0efd579a55ec0a964699f8c4022ad3
Scott Roelofs <scottroelofs@icloud.com>
2024-11-18 00:23:47 -0700

    Fixed vite config build options

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    assetsDir: 'assets',
     rollupOptions: {
-      external: [
-        'pdfjs-dist/build/pdf.worker.entry'
-      ],
       output: {
         manualChunks: {
           pdfjs: ['pdfjs-dist']
-        }
+        },
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
       }
     }
   },
@@ -23,5 +24,9 @@ export default defineConfig({
       'Cross-Origin-Embedder-Policy': 'require-corp',
       'Cross-Origin-Opener-Policy': 'same-origin'
     }
-  }
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
+  },
+  base: '/'
 });
