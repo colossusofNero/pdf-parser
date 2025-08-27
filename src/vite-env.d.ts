b33e9905c1c4da9f0f60917e405101991ab8cc97
colossusofNero <scottroelofs@icloud.com>
2025-08-27 09:34:03 -0700

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,12 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_CASPIO_API_KEY: string
+  readonly VITE_CASPIO_ACCESS_TOKEN: string
   readonly VITE_CASPIO_API_URL: string
+  readonly VITE_CASPIO_FILE_UPLOAD_URL: string
+  readonly DEV: boolean
+  readonly PROD: boolean
+  readonly MODE: string
 }
 
 interface ImportMeta {
