c95ac683fd70d5b43cafc72e2876dd5a29de8e0f
colossusofNero <scottroelofs@icloud.com>
2025-09-02 11:46:02 -0700

    Update ping.cjs

--- api/ping.cjs
+++ api/ping.cjs
@@ -1,4 +1,12 @@
-module.exports = (_req, res) => {
-  res.status(200).json({ ok: true, node: process.version, now: Date.now() });
+function send(res, code, body) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,HEAD');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.status(code).json(body);
+}
+module.exports = (req, res) => {
+  if (req.method === 'OPTIONS' || req.method === 'HEAD') return send(res, 204, {});
+  if (req.method !== 'GET') return send(res, 405, { error: 'Method Not Allowed' });
+  return send(res, 200, { ok: true, node: process.version, now: Date.now() });
 };
 module.exports.config = { runtime: 'nodejs' };
