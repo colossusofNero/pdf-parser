384c8a134f3c546c15c17fa32c792b0101c33b0e
colossusofNero <scottroelofs@icloud.com>
2025-09-02 12:29:21 -0700

    Update diag.cjs

--- api/diag.cjs
+++ api/diag.cjs
@@ -1,12 +1,12 @@
 function send(res, code, body) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,HEAD');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,OPTIONS,HEAD');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.status(code).json(body);
 }
 module.exports = async (req, res) => {
   if (req.method === 'OPTIONS' || req.method === 'HEAD') return send(res, 204, {});
-  if (req.method !== 'GET') return send(res, 405, { error: 'Method Not Allowed' });
+  if (req.method !== 'GET') return send(res, 405, { error: 'Method Not Allowed', method: req.method });
 
   const out = { ok: true, node: process.version };
   try { await import('googleapis'); out.googleapis = 'ok'; }
