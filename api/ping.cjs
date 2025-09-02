627c933ae9329ae107fe1572d2db62c8726a85e1
colossusofNero <scottroelofs@icloud.com>
2025-09-02 10:31:53 -0700

    Update and rename ping.ts to ping.cjs

--- api/ping.cjs
+++ api/ping.cjs
@@ -1,5 +1,4 @@
-// CommonJS, zero deps, cannot crash on import
-exports.config = { runtime: 'nodejs' };
 module.exports = (_req, res) => {
   res.status(200).json({ ok: true, node: process.version, now: Date.now() });
 };
+module.exports.config = { runtime: 'nodejs' };
